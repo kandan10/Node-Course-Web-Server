badb63c1eb91749d2f605456ad2b425780476602
Kandan <kandancd@gmail.com>
2018-07-04 08:53:05 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+app.set('view engine', 'hbs');
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = (`${now}: ${req.method} ${req.url}`);
+    console.log(log);
+    fs.appendFile('server.log', log +'/n', (error) => {
+        if(error){
+            console.log('Unable to write log');
+        }
+    });
+    next();
+});
+
+// app.use((req,res,next) => {
+//     res.render('maintainance.hbs');
+// });
+
+app.get('/', (req,res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'yo'
+    })
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send('Bad request');
+});
+
+app.listen(3000);
